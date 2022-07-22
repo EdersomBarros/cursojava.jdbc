fea720c8f846be7dfcfe3fb21f3a97627c9e6ce1
Perkles <edersombarros@hotmail.com>
2022-07-21 22:27:30 -0400

    conexão com banco

---
+++ src/test/java/apptest/TesteBancoJdbc.java
@@ -0,0 +1,14 @@
+package apptest;
+
+import org.junit.Test;
+
+import conexaojdbc.SingleConnection;
+
+public class TesteBancoJdbc {
+
+	@Test
+	public void initBanco() {
+		SingleConnection.getConnection();
+	}
+	
+}
