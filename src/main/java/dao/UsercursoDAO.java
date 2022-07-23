3d3456c17a0867a940e4382dc40be5af717702c4
Perkles <edersombarros@hotmail.com>
2022-07-23 13:33:55 -0400

    criação do insert

--- src/main/java/dao/UsercursoDAO.java
+++ src/main/java/dao/UsercursoDAO.java
@@ -1,8 +1,11 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import conexaojdbc.SingleConnection;
+import model.Usercursojava;
 
 public class UsercursoDAO {
 
@@ -12,6 +15,28 @@ public class UsercursoDAO {
 
 		connection = SingleConnection.getConnection();
 
+	}
+
+	public void salvar(Usercursojava usercursojava) {
+		try {
+			String sql = "insert into usercursojava (id,nome,email) values(?,?,?)";
+			PreparedStatement insert = connection.prepareStatement(sql);
+
+			insert.setLong(1, usercursojava.getId());
+			insert.setString(2, usercursojava.getNome());
+			insert.setString(3, usercursojava.getEmail());
+			insert.execute();
+			connection.commit(); //salva no banco
+			
+		} catch (Exception e) {
+			try {
+				connection.rollback(); //reverte a operação
+			} catch (SQLException e1) {
+				
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/test/java/apptest/TesteBancoJdbc.java
+++ src/test/java/apptest/TesteBancoJdbc.java
@@ -3,12 +3,22 @@ package apptest;
 import org.junit.Test;
 
 import conexaojdbc.SingleConnection;
+import dao.UsercursoDAO;
+import model.Usercursojava;
 
 public class TesteBancoJdbc {
 
 	@Test
 	public void initBanco() {
-		SingleConnection.getConnection();
+
+		UsercursoDAO usercursoDAO = new UsercursoDAO();
+		Usercursojava usercursojava = new Usercursojava();
+
+		usercursojava.setId(6L);
+		usercursojava.setNome("Johnva");
+		usercursojava.setEmail("jahho@hotmail.com");
+
+		usercursoDAO.salvar(usercursojava);
 	}
-	
+
 }
